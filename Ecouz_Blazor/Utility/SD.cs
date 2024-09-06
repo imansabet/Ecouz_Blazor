c42610f6d8ba9cff91ff3566dfa5681940bfc239
iman <dev.imansabet@gmail.com>
2024-09-06 13:28:00 +0330

    convert shopping cart to order details

--- Ecouz_Blazor/Utility/SD.cs
+++ Ecouz_Blazor/Utility/SD.cs
@@ -1,4 +1,6 @@
-﻿namespace Ecouz_Blazor.Utility;
+﻿using Ecouz_Blazor.Data;
+
+namespace Ecouz_Blazor.Utility;
 
 public static class SD
 {
@@ -10,4 +12,25 @@ public static class SD
     public static string StatusReadyForPickUp = "ReadyForPickUp";
     public static string StatusCompleted = "Completed";
     public static string StatusCancelled = "Cancelled";
+
+
+	public static List<OrderDetail> ConvertShoppingCartListToOrderDetail(List<ShoppingCart> shoppingCarts)
+	{
+		List<OrderDetail> orderDetails = new List<OrderDetail>();
+
+		foreach (var cart in shoppingCarts)
+		{
+			OrderDetail orderDetail = new OrderDetail
+			{
+				ProductId = cart.ProductId,
+				Count = cart.Count,
+				Price = Convert.ToDouble(cart.Product.Price),
+				ProductName = cart.Product.Name
+			};
+			orderDetails.Add(orderDetail);
+		}
+
+		return orderDetails;
+	}
 }
+    
